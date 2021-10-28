fd582ad85eae01e74d63fb647c4a4c028e14e706
Yaremko Ivan <yaremich98@gmail.com>
2021-10-28 09:58:46 +0300

    Add SwitchInnerText

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,23 @@
 
 const [openBtn, closeBtn] = document.querySelectorAll('button');
 
-const switchInnerText = () =>{}
+const switchInnerText = ({target}) =>{
+    const openElem = target === closeBtn ? openBtn : closeBtn; 
+    const closeElem = target === closeBtn ? closeBtn : openBtn; 
+    const buffer = openElem.textContent;
+    openElem.textContent = closeElem.textContent;
+    closeElem.textContent = buffer;
 
+    closeElem.removeEventListener('mouseenter', switchInnerText);
+    openElem.addEventListener('mouseenter', switchInnerText);
 
-openBtn.addEventListener('mouseenter');
-closeBtn.addEventListener('mouseenter');
+}
+const clickHandler = ({target})=>{
+    console.log(target);
+}
+
+//openBtn.addEventListener('mouseenter', switchInnerText, {once:true});
+closeBtn.addEventListener('mouseenter', switchInnerText);
+
+openBtn.addEventListener('click', clickHandler);
+closeBtn.addEventListener('click', clickHandler);
