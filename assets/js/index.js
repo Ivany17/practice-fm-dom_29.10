45dbaf9a6786c52c94bb6c6425db0f0164304a4e
Yaremko Ivan <yaremich98@gmail.com>
2021-10-28 09:22:55 +0300

    Start of task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const [openBtn, closeBtn] = document.querySelectorAll('button');
+
+const switchInnerText = () =>{}
+
+
+openBtn.addEventListener('mouseenter');
+closeBtn.addEventListener('mouseenter');
