07051297c2802386cdf7cf224115c1ae4c42043e
Yaremko Ivan <yaremich98@gmail.com>
2021-10-28 10:28:22 +0300

    Data atributes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,9 @@
 'use strict'
 
-const [openBtn, closeBtn] = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button');
 
-const switchInnerText = ({target}) =>{
-    const openElem = target === closeBtn ? openBtn : closeBtn; 
-    const closeElem = target === closeBtn ? closeBtn : openBtn; 
-    const buffer = openElem.textContent;
-    openElem.textContent = closeElem.textContent;
-    closeElem.textContent = buffer;
+const btnHandler = ({target:{dataset:{path}}})=>{console.log(path);}
 
-    closeElem.removeEventListener('mouseenter', switchInnerText);
-    openElem.addEventListener('mouseenter', switchInnerText);
-
+for (const btn of btns) {
+    btn.addEventListener('click', btnHandler);
 }
-const clickHandler = ({target})=>{
-    console.log(target);
-}
-
-//openBtn.addEventListener('mouseenter', switchInnerText, {once:true});
-closeBtn.addEventListener('mouseenter', switchInnerText);
-
-openBtn.addEventListener('click', clickHandler);
-closeBtn.addEventListener('click', clickHandler);
