84308eab67f6b0cfa73c432eceafc58f952a386a
Yaremko Ivan <yaremich98@gmail.com>
2021-10-29 09:16:02 +0300

    Add data.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,24 @@
 
 const btns = document.querySelectorAll('button');
 
-const btnHandler = ({target:{dataset:{path}}})=>{console.log(path);}
+const btnHandler = ({
+    target:{
+        dataset:{color},
+        parentNode: parentRoot
+    }
+})=>{
+    parentRoot.style.backgroundColor = color;
+}
+const handler = (e)=>{console.log(e)}
 
 for (const btn of btns) {
     btn.addEventListener('click', btnHandler);
+    btn.addEventListener('click', handler, true);
 }
+
+document.getElementById('root').addEventListener('click', handler);
+document.body.addEventListener('click', handler);
+
+document.addEventListener('click', handler);
+window.addEventListener('click', handler, true);
+
