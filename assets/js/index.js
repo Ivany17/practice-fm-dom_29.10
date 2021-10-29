a165bd202ab47433e97008de0061b57b4a3c6820
Yaremko Ivan <yaremich98@gmail.com>
2021-10-29 11:09:11 +0300

    Refuctor HTML card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
 'use strict'
+
+const cardsContainer = document.getElementById('cardsContainer');
+
+const HTMLElements = actors.map((actor)=>createActorCards(actor));
+
+function createActorCards(actor){
+    const card = document.createElement('li');
+    card.classList.add('cardWrapper');
+
+    const container = document.createElement('article');
+    container.classList.add('cardContainer');
+
+    const img = document.createElement('img');
+    img.classList.add('cardImage');
+    img.setAttribute('src', actor.photo);
+    img.setAttribute('alt', actor.name);
+
+
+    const name = document.createElement('h2');
+    name.classList.add('cardName');
+    name.append(document.createTextNode(actor.name));
+
+    const description = document.createElement('p');
+    name.classList.add('cardDescription');
+    description.append(document.createTextNode(actor.birthdate));
+    
+    container.append(img, name, description);
+    card.appendChild(container);
+
+    return card;
+}
+
+// for (const elem of HTMLElements) {
+//     cardsContainer.appendChild(elem);
+// }
+
+//cardsContainer.append(...HTMLElements)
