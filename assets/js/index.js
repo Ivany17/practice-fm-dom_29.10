7e210833a3529735617046cd58316c97713b1e59
Yaremko Ivan <yaremich98@gmail.com>
2021-11-01 10:50:12 +0200

    Refactor ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,89 @@
-'use strict'
-
+'use strict';
 const cardsContainer = document.getElementById('cardsContainer');
+const HTMLElements = actors
+    .filter((actor)=>actor.name&&actor.birthdate&&actor.photo)
+    .map((actor)=>createActorCards(actor));
 
-const HTMLElements = actors.map((actor)=>createActorCards(actor));
 
 function createActorCards(actor){
-    const card = document.createElement('li');
-    card.classList.add('cardWrapper');
+  const p = createElement('p', {classNames:['cardDescription']},
+        document.createTextNode(actor.birthdate || 'unknow')
+      );
+  const h2 = createElement('h2', {classNames:['cardName']},
+        document.createTextNode(actor.name || 'noname')
+      );
+  const article = createElement('article', {classNames:['cardContainer']}, 
+        createImageWrapper(actor),
+        h2,
+        p,
+      );
+  return createElement('li',{classNames:['cardWrapper']}, article);
+}
+cardsContainer.append(...HTMLElements);
 
-    const container = document.createElement('article');
-    container.classList.add('cardContainer');
-
-    const imgWrapper = document.createElement('div');
-    imgWrapper.classList.add('cardImageWrapper');
-
-    const initials = document.createElement('div');
-    initials.classList.add('initials');
-    initials.append(document.createTextNode(actor.name[0] || 'noname'));
-    initials.style.backgroundColor = stringToColour(actor.name || '');
-
-    const img = document.createElement('img');
-    img.classList.add('cardImage');
-    img.setAttribute('src', actor.photo);
-    img.setAttribute('alt', actor.name);
-    img.addEventListener('error', handleImageError);
-
-    imgWrapper.append(initials, img);
-
-    const name = document.createElement('h2');
-    name.classList.add('cardName');
-    name.append(document.createTextNode(actor.name || 'noname'));
-
-    const description = document.createElement('p');
-    name.classList.add('cardDescription');
-    description.append(document.createTextNode(actor.birthdate));
-    
-    container.append(imgWrapper, name, description);
-    card.appendChild(container);
-    return card;
+/**
+ * 
+ * @param {string} type 
+ * @param {object} options 
+ * @param {Node[]} children 
+ */
+function createElement(type,{classNames, onClick}, ...children){
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.addEventListener('click', onClick);//for example
+  elem.append(...children);
+  return elem;
 }
 
-cardsContainer.append(...HTMLElements)
+function createImageWrapper(actor){
+  const {id, name} = actor;
+  const imgWrapper = document.createElement('div');
+  imgWrapper.setAttribute('id', `wrapper${id}`);
+  imgWrapper.classList.add('cardImageWrapper');
 
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(name[0] || 'noname'));
+  initials.style.backgroundColor = stringToColour(name || '');
+
+  imgWrapper.append(initials, createImage(actor));
+  return imgWrapper;
+}
+
+function createImage({photo, name, id}){
+  const img = document.createElement('img');
+  img.dataset.id = id;  
+  img.classList.add('cardImage');
+  img.setAttribute('src', photo);
+  img.setAttribute('alt', name);
+  img.addEventListener('error', handleImageError);
+  img.addEventListener('load', handleImageLoad);
+  return img;
+}
+
+/*  handles */
 function handleImageError({target}){
-    target.remove();
+  target.remove();
+}
+function handleImageLoad({
+  target, 
+  target:{
+    dataset:{id},
+  }
+}){
+  document.getElementById(`wrapper${id}`).append(target);
 }
 
+/* utilits */
 function stringToColour(str) {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++){
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    let colour = '#';
-    for (let i = 0; i < 3; i++){
-        let value = (hash >> (i * 8))& 0xFF;
-        colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
-}
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}  
